21d6c20a427829f565d844c08fca417691941973
SemenSavlook <semensavluk@gmail.com>
2020-11-29 17:27:32 +0600

    Refactoring 2

--- Project 1/js/script.js
+++ Project 1/js/script.js
@@ -1,29 +1,23 @@
 'use strict';
 
-let numberOfFilms;
-
-// function start() {
-// 	numberOfFilms	= +prompt('Сколько фильмов вы уже посмотрели?', '10');
-// 	while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms) ) {
-// 		numberOfFilms	= +prompt('Сколько фильмов вы уже посмотрели?', '');
-// 	}
-// }
-
-// start();
-
 const personalMovieDB = {
-	count: numberOfFilms,
+	count: 0,
 	movies: {},
 	actors: {},
 	genres: [],
 	privat: false,
+
+	start: function () {
+		personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '10');
+		while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+			personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+		}
+	},
+
 	rememberMyFilms: function () {
-
 		for (let i = 0; i < 2; i++) {
-
 			const a = prompt('Один из последних просмотренных фильмов?', 'Чики'),
 				b = prompt('На сколько оцените его?', 5);
-
 			if (a != null && b != null && a != '' && b != '' && a.length < 50) {
 				personalMovieDB.movies[a] = b;
 			} else {
@@ -33,7 +27,6 @@ const personalMovieDB = {
 	},
 
 	detectPersonalLevel: function () {
-
 		if (personalMovieDB.count < 10) {
 			alert('Просмотрено довольно мало фильмов');
 		} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
@@ -46,31 +39,44 @@ const personalMovieDB = {
 	},
 
 	showMyDB: function (hidden) {
-		if (!hidden) {
-		console.log(personalMovieDB);
+		if (hidden) {
+			console.log(personalMovieDB);
+		} else {console.log('База скрыта');}
+	},
+
+	toggleVisibleMyDB: function (checker) {
+		if (personalMovieDB.privat) {
+			personalMovieDB.privat = false;
+		} else {
+			personalMovieDB.privat = true;
 		}
 	},
 
 	writeYourGenres: function () {
-		for (let i = 1; i <= 3; i++) {
-			personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`, 'Боевик');
-			if (personalMovieDB.genres[i - 1] == null || personalMovieDB.genres[i - 1] == '') {
-				i--;
+		for (let i = 1; i < 2; i++) {	
+			let genres = prompt(`Введите ваши любимые жанры, через запятую`, 'Боевик, Комедия, Триллер');
+				if (genres == null || genres == '') {
+					i--;
+				} else {
+					personalMovieDB.genres = genres.split(', ');
+					}
 			}
-		}
-	},
-
-	toggleVisibleMyDB: function(checker) {
-		if (checker) { this.privat = false; } else { this.privat = true; }
+		personalMovieDB.genres.forEach((item, index) => {
+			
+			console.log(`Любимый жанр #${index + 1}: ${item}`);
+		});
 	}
+
 };
 
+personalMovieDB.showMyDB(personalMovieDB.privat);
 personalMovieDB.writeYourGenres();
+personalMovieDB.toggleVisibleMyDB();
+personalMovieDB.showMyDB(personalMovieDB.privat);
 
 
-personalMovieDB.genres.forEach (function (cvalue, index) {
-	console.log(`Любимый жанр #${index+1}: ${cvalue}`);
-});
+
+
 
 
 
