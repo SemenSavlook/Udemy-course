f177e058f2d23d1cc0cbd0208161652cf86f7931
SemenSavlook <semensavluk@gmail.com>
2020-12-08 19:00:39 +0600

    Commit from vscode 044lesson

--- 038 Food-dist/js/script.js
+++ 038 Food-dist/js/script.js
@@ -5,10 +5,10 @@ window.addEventListener('DOMContentLoaded', () => {
 	//TABS ------------------------ ------------------------ ------------------------
 
 	const tabs = document.querySelectorAll('.tabheader__item'),
-				tabsContent = document.querySelectorAll('.tabcontent'),
-				tabsParent = document.querySelector('.tabheader__items');
+		tabsContent = document.querySelectorAll('.tabcontent'),
+		tabsParent = document.querySelector('.tabheader__items');
 
-	function hideTabContent () {
+	function hideTabContent() {
 		tabsContent.forEach(item => {
 			item.classList.add('hide');
 			item.classList.remove('show', 'fade');
@@ -32,12 +32,12 @@ window.addEventListener('DOMContentLoaded', () => {
 		const target = event.target;
 
 		if (target && target.classList.contains('tabheader__item')) {
-				tabs.forEach((item, i) => {
-					if (target == item) {
-						hideTabContent();
-						showTabContent(i);
-					}
-				});
+			tabs.forEach((item, i) => {
+				if (target == item) {
+					hideTabContent();
+					showTabContent(i);
+				}
+			});
 		}
 	});
 
@@ -47,10 +47,10 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	function getTimeRemaining(endtime) {
 		const t = Date.parse(endtime) - Date.parse(new Date()),
-					days = Math.floor(t / (1000*60*60*24)),
-					hours = Math.floor((t / (1000*60*60) % 24)),
-					minutes = Math.floor((t/1000/60) % 60),
-					seconds = Math.floor((t/1000) % 60);
+			days = Math.floor(t / (1000 * 60 * 60 * 24)),
+			hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+			minutes = Math.floor((t / 1000 / 60) % 60),
+			seconds = Math.floor((t / 1000) % 60);
 
 		return {
 			'total': t,
@@ -71,11 +71,11 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	function setClock(selector, endtime) {
 		const timer = document.querySelector(selector),
-		days = timer.querySelector('#days'),
-		hours = timer.querySelector('#hours'),
-		minutes = timer.querySelector('#minutes'),
-		seconds = timer.querySelector('#seconds'),
-		timeInterval = setInterval (updateClock, 1000);
+			days = timer.querySelector('#days'),
+			hours = timer.querySelector('#hours'),
+			minutes = timer.querySelector('#minutes'),
+			seconds = timer.querySelector('#seconds'),
+			timeInterval = setInterval(updateClock, 1000);
 
 		updateClock();
 
@@ -95,5 +95,53 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	setClock('.timer', deadline);
 
-// END-END____------
+
+	//Modal window ---------------------------------------
+
+	const modalTrigger = document.querySelectorAll('[data-modal]'),
+				modal = document.querySelector('.modal'),
+				modalCloseBtn = document.querySelector('[data-close]');
+		
+	function closeModal() {
+		modal.classList.toggle('show');
+		document.body.style.overflow = '';
+		}
+
+	modalTrigger.forEach(btn => {
+		btn.addEventListener('click', openModal);
+	});
+
+	function openModal() {
+		modal.classList.toggle('show');
+		document.body.style.overflow = 'hidden';
+		clearInterval(modalTimerId);
+	}
+
+	modalCloseBtn.addEventListener('click', closeModal);
+
+	modal.addEventListener('click', (e) => {
+		if (e.target === modal) {
+			closeModal();
+		}
+	});
+
+	document.addEventListener('keydown', (e) => {
+		if (e.code == 'Escape' && modal.classList.contains('show')) {
+			closeModal();
+		}
+	});
+
+	const modalTimerId = setTimeout(openModal, 10000);
+
+	function showModalByScroll() {
+		if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+			openModal();
+			window.removeEventListener('scroll', showModalByScroll);
+		}
+	}
+		
+	window.addEventListener('scroll', showModalByScroll);
+
+
+	// END-END____------ ---------------------------------------
 });
