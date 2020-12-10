fa38d9f442a2c62cbff34f346e9b6f8b25ac26d2
SemenSavlook <semensavluk@gmail.com>
2020-12-10 18:50:22 +0600

    049 Lesson from work

--- 038 Food-dist/js/script.js
+++ 038 Food-dist/js/script.js
@@ -133,7 +133,7 @@ window.addEventListener('DOMContentLoaded', () => {
 		}
 	});
 
-	const modalTimerId = setTimeout(openModal, 20000);
+	const modalTimerId = setTimeout(openModal, 200000);
 
 	function showModalByScroll() {
 		if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
@@ -145,15 +145,16 @@ window.addEventListener('DOMContentLoaded', () => {
 	window.addEventListener('scroll', showModalByScroll);
 
 
-	// Создание классов для меню карточек
+	// Создание класса для меню карточек
 
 	class MenuCard {
-		constructor(src, alt, title, descr, price, parentSelector) {
+		constructor(src, alt, title, descr, price, parentSelector, ...classes) {
 			this.src = src;
 			this.alt = alt;
 			this.title = title;
 			this.descr = descr;
 			this.price = price;
+			this.classes = classes;
 			this.parent = document.querySelector(parentSelector);
 			this.transfer = 420;
 			this.changeToKZT();
@@ -165,33 +166,42 @@ window.addEventListener('DOMContentLoaded', () => {
 
 		render() {
 			const element = document.createElement('div');
+
+			if (this.classes.length === 0) {
+				this.element = 'menu__item';
+				element.classList.add(this.element);
+			} else {
+				this.classes.forEach(className => element.classList.add(className));
+			}
+
 			element.innerHTML = `
-				<div class="menu__item">
-					<img src=${this.src} alt=${this.alt}>
-					<h3 class="menu__item-subtitle">${this.title}</h3>
-					<div class="menu__item-descr">${this.descr}</div>
-					<div class="menu__item-divider"></div>
-					<div class="menu__item-price">
-						<div class="menu__item-cost">Цена:</div>
-						<div class="menu__item-total"><span>${this.price}</span> тг/день</div>
-					</div>
+				<img src=${this.src} alt=${this.alt}>
+				<h3 class="menu__item-subtitle">${this.title}</h3>
+				<div class="menu__item-descr">${this.descr}</div>
+				<div class="menu__item-divider"></div>
+				<div class="menu__item-price">
+					<div class="menu__item-cost">Цена:</div>
+					<div class="menu__item-total"><span>${this.price}</span> тг/день</div>
 				</div>
+
 			`;
 			this.parent.append(element);
 		}
 	}
 
-// ---------Dynamic Card -
+	// ---------Dynamic Card -
 	new MenuCard(
 		"img/tabs/vegy.jpg",
 		"vegy",
 		'Меню "Фитнес"',
 		'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов.Продукт активных и здоровых людей.Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
 		8,
-		'.menu .container'
+		'.menu .container',
+		'menu__item',
+		'big'
 	).render();
 
-// ---------Dynamic Card -
+	// ---------Dynamic Card -
 	new MenuCard(
 		"img/tabs/elite.jpg",
 		"elite",
@@ -201,15 +211,18 @@ window.addEventListener('DOMContentLoaded', () => {
 		'.menu .container'
 	).render();
 
-// ---------Dynamic Card -
+	// ---------Dynamic Card -
 	new MenuCard(
 		"img/tabs/post.jpg",
 		"postnoe",
 		'Меню "Постное"',
 		'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
 		6,
-		'.menu .container'
+		'.menu .container',
+		'menu__item',
+		'big'
 	).render();
 
 	// END-END____------ ---------------------------------------
 });
+
